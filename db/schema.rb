3e8017a742f4385f2ad28166e9cdf3f4c59f4bb4
haliespaige <halie.romasco@gmail.com>
2023-06-22 10:22:19 -0700

    Finished Rails API Initial Config, Devise dependencies, JWT config

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_21_221904) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_22_171634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "area"
