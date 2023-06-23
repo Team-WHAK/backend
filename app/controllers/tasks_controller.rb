06a4092e131a84292bac91e07ce83646bd2e1a3b
wabowen89 <wabowen89@gmail.com>
2023-06-23 12:01:33 -0700

    added rspec validations for update as well as appropriate request validations for update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,33 @@
 class TasksController < ApplicationController
-    def create
-        task = Task.create(task_params)
-        if task.valid?
-            render json: task
-        else 
-            render json: task.errors, status: 422
-        end
-    end
     
-    private
+  def index
+    tasks = Task.all
+    render json: tasks
+  end
+
+  def create
+    task = Task.create(task_params)
+      if task.valid?
+        render json: task
+      else 
+        render json: task.errors, status: 422
+      end
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+      if task.valid?
+        render json: task
+      else
+        render json: task.errors, status: 422
+      end
+  end
     
-    def task_params
-        params.require(:task).permit(:area, :item, :picture, :task_name, :task_descr, :frequency, :due_date)
-    end
+  private
+    
+  def task_params
+    params.require(:task).permit(:area, :item, :picture, :task_name, :task_descr, :frequency, :due_date)
+  end
+
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
+
+  let(:user) { User.create(
+    email: 'test@example.com',
+    password: 'password',
+    password_confirmation: 'password'
+    )
+  }
+
   describe "POST /create" do
     it "doesn't create a task without an area" do
       task_params = {
@@ -129,5 +137,297 @@ RSpec.describe "Tasks", type: :request do
 
 
     end
+  end
+
+  describe "PATCH /update" do
+
+    it "updates a task with valid attributes" do
+    
+      task = user.tasks.create(
+        area: 'Backyard',
+        item: 'Stove',
+        picture: 'img',
+        task_name: 'clean',
+        task_descr: 'wipe down surface',
+        frequency: 'daily',
+        due_date: '23 Aug 2023' 
+      )
+
+      get '/tasks'
+
+      updTask = Task.first
+      updTask_params ={
+        task: {
+          area: 'Porch',
+          item: 'Microwave',
+          picture: 'images',
+          task_name: 'break',
+          task_descr: 'snap in half',
+          frequency: 'monthly',
+          due_date: '17 Jul 2023',
+          user_id: user.id
+        }
+      }
+
+      patch "/tasks/#{updTask.id}", params: updTask_params
+      task = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+
+      expect(task['area']).to eq('Porch')
+      expect(task['item']).to eq('Microwave')
+      expect(task['picture']).to eq('images')
+      expect(task['task_name']).to eq('break')
+      expect(task['task_descr']).to eq('snap in half')
+      expect(task['frequency']).to eq('monthly')
+      expect(task['due_date']).to eq('17 Jul 2023')
+    
+    end
+
+    it "doesn't update a task without an area" do
+      task = user.tasks.create(
+        area: 'Backyard',
+        item: 'Stove',
+        picture: 'img',
+        task_name: 'clean',
+        task_descr: 'wipe down surface',
+        frequency: 'daily',
+        due_date: '23 Aug 2023' 
+      )
+
+      get '/tasks'
+
+      updTask = Task.first
+      updTask_params ={
+        task: {
+          area: nil,
+          item: 'Stove',
+          picture: 'img',
+          task_name: 'clean',
+          task_descr: 'wipe down surface',
+          frequency: 'daily',
+          due_date: '23 Aug 2023', 
+          user_id: user.id
+        }
+      }
+
+      patch "/tasks/#{updTask.id}", params: updTask_params
+      task = JSON.parse(response.body)
+
+      expect(response).to have_http_status(422)   
+      expect(task['area']).to include("can't be blank") 
+      
+    end
+
+    it "doesn't update a task without an item" do
+      task = user.tasks.create(
+        area: 'Backyard',
+        item: 'Stove',
+        picture: 'img',
+        task_name: 'clean',
+        task_descr: 'wipe down surface',
+        frequency: 'daily',
+        due_date: '23 Aug 2023' 
+      )
+
+      get '/tasks'
+
+      updTask = Task.first
+      updTask_params ={
+        task: {
+          area: 'Backyard',
+          item: nil,
+          picture: 'img',
+          task_name: 'clean',
+          task_descr: 'wipe down surface',
+          frequency: 'daily',
+          due_date: '23 Aug 2023', 
+          user_id: user.id
+        }
+      }
+
+      patch "/tasks/#{updTask.id}", params: updTask_params
+      task = JSON.parse(response.body)
+
+      expect(response).to have_http_status(422)   
+      expect(task['item']).to include("can't be blank") 
+      
+    end
+
+    it "doesn't update a task without a picture" do
+      task = user.tasks.create(
+        area: 'Backyard',
+        item: 'Stove',
+        picture: 'img',
+        task_name: 'clean',
+        task_descr: 'wipe down surface',
+        frequency: 'daily',
+        due_date: '23 Aug 2023' 
+      )
+
+      get '/tasks'
+
+      updTask = Task.first
+      updTask_params ={
+        task: {
+          area: 'Backyard',
+          item: 'Stove',
+          picture: nil,
+          task_name: 'clean',
+          task_descr: 'wipe down surface',
+          frequency: 'daily',
+          due_date: '23 Aug 2023', 
+          user_id: user.id
+        }
+      }
+
+      patch "/tasks/#{updTask.id}", params: updTask_params
+      task = JSON.parse(response.body)
+
+      expect(response).to have_http_status(422)   
+      expect(task['picture']).to include("can't be blank") 
+      
+    end
+
+    it "doesn't update a task without a task_name" do
+      task = user.tasks.create(
+        area: 'Backyard',
+        item: 'Stove',
+        picture: 'img',
+        task_name: 'clean',
+        task_descr: 'wipe down surface',
+        frequency: 'daily',
+        due_date: '23 Aug 2023' 
+      )
+
+      get '/tasks'
+
+      updTask = Task.first
+      updTask_params ={
+        task: {
+          area: 'Backyard',
+          item: 'Stove',
+          picture: 'img',
+          task_name: nil,
+          task_descr: 'wipe down surface',
+          frequency: 'daily',
+          due_date: '23 Aug 2023', 
+          user_id: user.id
+        }
+      }
+
+      patch "/tasks/#{updTask.id}", params: updTask_params
+      task = JSON.parse(response.body)
+
+      expect(response).to have_http_status(422)   
+      expect(task['task_name']).to include("can't be blank") 
+      
+    end
+
+    it "doesn't update a task without a task_descr" do
+      task = user.tasks.create(
+        area: 'Backyard',
+        item: 'Stove',
+        picture: 'img',
+        task_name: 'clean',
+        task_descr: 'wipe down surface',
+        frequency: 'daily',
+        due_date: '23 Aug 2023' 
+      )
+
+      get '/tasks'
+
+      updTask = Task.first
+      updTask_params ={
+        task: {
+          area: 'Backyard',
+          item: 'Stove',
+          picture: 'img',
+          task_name: 'clean',
+          task_descr: nil,
+          frequency: 'daily',
+          due_date: '23 Aug 2023', 
+          user_id: user.id
+        }
+      }
+
+      patch "/tasks/#{updTask.id}", params: updTask_params
+      task = JSON.parse(response.body)
+
+      expect(response).to have_http_status(422)   
+      expect(task['task_descr']).to include("can't be blank") 
+      
+    end
+
+    it "doesn't update a task without a frequency" do
+      task = user.tasks.create(
+        area: 'Backyard',
+        item: 'Stove',
+        picture: 'img',
+        task_name: 'clean',
+        task_descr: 'wipe down surface',
+        frequency: 'daily',
+        due_date: '23 Aug 2023' 
+      )
+
+      get '/tasks'
+
+      updTask = Task.first
+      updTask_params ={
+        task: {
+          area: 'Backyard',
+          item: 'Stove',
+          picture: 'img',
+          task_name: 'clean',
+          task_descr: 'wipe down surface',
+          frequency: nil,
+          due_date: '23 Aug 2023', 
+          user_id: user.id
+        }
+      }
+
+      patch "/tasks/#{updTask.id}", params: updTask_params
+      task = JSON.parse(response.body)
+
+      expect(response).to have_http_status(422)   
+      expect(task['frequency']).to include("can't be blank") 
+      
+    end
+
+    it "doesn't update a task without a due_date" do
+      task = user.tasks.create(
+        area: 'Backyard',
+        item: 'Stove',
+        picture: 'img',
+        task_name: 'clean',
+        task_descr: 'wipe down surface',
+        frequency: 'daily',
+        due_date: '23 Aug 2023' 
+      )
+
+      get '/tasks'
+
+      updTask = Task.first
+      updTask_params ={
+        task: {
+          area: 'Backyard',
+          item: 'Stove',
+          picture: 'img',
+          task_name: 'clean',
+          task_descr: 'wipe down surface',
+          frequency: 'daily',
+          due_date: nil, 
+          user_id: user.id
+        }
+      }
+
+      patch "/tasks/#{updTask.id}", params: updTask_params
+      task = JSON.parse(response.body)
+
+      expect(response).to have_http_status(422)   
+      expect(task['due_date']).to include("can't be blank") 
+      
+    end
+
   end
 end
