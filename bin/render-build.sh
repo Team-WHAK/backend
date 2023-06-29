41345be4f8c46e262856f4212c11ef9d6efa7d7c
haliespaige <halie.romasco@gmail.com>
2023-06-29 14:44:12 -0700

    commented out line 7

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rake db:seed
+# bundle exec rake db:seed
 # remove or comment out the line above after initial build
